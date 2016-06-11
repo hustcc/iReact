43ce654e67e7aad537c0b1fec57748321dc35fc3
aTool <i@atool.org>
2016-06-11 14:30:12 +0800

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,11 @@
 const fs = require('fs');
 const gulp = require('gulp');
 const wrap = require('gulp-wrap');
-const header = require('gulp-header');
-const pkg = require('./package.json');
 const uglify = require('gulp-uglify');
-const banner = `\
-/**
- * <%= pkg.title %> - <%= pkg.description %>
- * @version v<%= pkg.version %>
- * @license <%= pkg.license %>
- * @author <%= pkg.author %>
- *
- * Copyright (c) 2016 hustcc
- * URL: http://www.iReact.cn/
- */
-`;
 
 gulp.task('build_data', () => (
   gulp.src('data.js')
   .pipe(wrap({src: 'res/js/exports.js'}))
-  .pipe(header(banner, {pkg: pkg}))
   .pipe(uglify())    //uglify
   .pipe(gulp.dest('res/js/dist/'))
 ));
